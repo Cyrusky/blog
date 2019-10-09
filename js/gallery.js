2b7188d0a281cb03bf860ce10e1dac67c8411534
金博 <bo.jin@borgor.cn>
2019-10-09 08:59:59 +0800

    Site updated: 2019-10-09 08:59:58

---
+++ js/gallery.js
@@ -0,0 +1,19 @@
+$(function(){
+    if (typeof ($.fn.justifiedGallery) === 'function') {
+      let justifiedGallery = $('.justified-gallery')
+      if(justifiedGallery.length > 0) {
+        for (let i = 0; i < justifiedGallery.length; i ++){
+          let html = justifiedGallery[i].childNodes[0].innerHTML
+          justifiedGallery[i].innerHTML = html
+        }
+      }
+      justifiedGallery.justifiedGallery({
+        cssAnimation: true,
+        imagesAnimationDuration: 1000
+      });
+    }
+    if (typeof ($.fn.lightGallery) === 'function') {
+        $('.article').lightGallery({ selector: '.gallery-item' });
+    }
+    $('p:has(.gallery-item)').css({'text-align': 'center'});
+})
