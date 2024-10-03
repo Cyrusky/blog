01f4a38c468ef8792d580479c3f2ec0d9ffa3df3
Bo.Jin <bo.jin@borgor.cn>
2024-10-03 11:13:31 +0800

    fix workflow

--- packages/admin/vite.config.ts
+++ packages/admin/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: "/admin/",
   build: {
-    outDir: "../../public/admin",
+    outDir: "../blogSource/public/admin",
   },
   css: {
     preprocessorOptions: {
