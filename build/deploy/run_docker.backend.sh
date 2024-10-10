663a1d9df6053433ab447f7f1701099f72392552
Bo.Jin <bo.jin@borgor.cn>
2024-10-10 22:01:59 +0800

    deploy backend

---
+++ build/deploy/run_docker.backend.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cd /root/runner/assistant/
+docker compose up -d
+if docker images | grep -q none; then
+  docker images | grep none | awk '{print $3}' | xargs docker image rm -f
+fi
