4727037e4cc89f9be5858456fe4ca386596a6c15
Bo.Jin <bo.jin@borgor.cn>
2024-10-10 22:34:10 +0800

    fix prisma

--- build/deploy/run_docker.backend.sh
+++ build/deploy/run_docker.backend.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd /root/runner/assistant/
+cd /root/runner/assistant_backend/
 docker compose up -d
 if docker images | grep -q none; then
   docker images | grep none | awk '{print $3}' | xargs docker image rm -f
